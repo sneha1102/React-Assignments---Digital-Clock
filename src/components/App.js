6ad0354ebd003df3df250f78a42c1b698b5e7116
sneha1102 <36694331+sneha1102@users.noreply.github.com>
2020-11-01 00:47:41 +0530

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -5,22 +5,12 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      time: new Date().toLocaleDateString("en-US", {
-        hour: "numeric",
-        minute: "numeric",
-        second: "numeric",
-        hour12: true
-      })
+      time: new Date().toLocaleString()
     };
   }
   tick() {
     this.setState({
-      time: new Date().toLocaleDateString("en-US", {
-        hour: "numeric",
-        minute: "numeric",
-        second: "numeric",
-        hour12: true
-      })
+      time: new Date().toLocaleString()
     });
   }
   componentDidMount() {
